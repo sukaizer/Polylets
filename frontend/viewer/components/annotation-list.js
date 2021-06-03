e7fdbfb73d2dba7e4b38d04b9b7eb7fadebeb84a
exsitu <exsitu@exsitus-MacBook-Pro.local>
2021-06-03 09:11:34 +0200

    selection fixed

--- frontend/viewer/components/annotation-list.js
+++ frontend/viewer/components/annotation-list.js
@@ -1,67 +1,102 @@
 app.component('annotation-list', {
     /*html*/
-    emits: ["update","del"],
-    props: ['note','current'],
-    
+    emits: ["update", "del"],
+    props: ['note', 'current'],
+
+    data() {
+        return {
+            toggled: true
+        }
+    },
+
     // template of the html code for the display of the passage object
     template: `
         <template v-if="note.fileId === current">
             <div class="passage">
-                <a :href=" '#' + note.id" title="retrace quote" class="notes" @click="selectText(note.id)" style="text-decoration: none"> {{ note.passage }} </a>
-                <button class="delete" @click="del(note.id)"> DELETE </button>
-                <button class="collapsible"> Note </button>
-                <div class="content">
-                    <textarea :id= "note.id" class="edit" placeholder="Type here" @input="change($event.target.value,note.id)"> {{ note.annotation }} </textarea>
-                </div>
+                <a :href=" '#' + note.id" title="retrace quote" class="notes" @click="selectText(note)" style="text-decoration: none"> {{ note.passage }} </a>
+                <button class="delete" @click="del(note.locId)"> DELETE </button>
+                <button class="collapsible" @click="toggle"> Note </button>
+                <template v-if="toggled">
+                    <div class="content">
+                        <textarea :id= "note.id" class="edit" placeholder="Type here" @input="change($event.target.value,note.id)"> {{ note.annotation }} </textarea>
+                    </div>
+                </template>
             </div>
-
-            <script>
-                var coll = document.getElementsByClassName("collapsible");
-                var i;
-
-                for (i = 0; i < coll.length; i++) {
-                    coll[i].addEventListener("click", function() {
-                        this.classList.toggle("active");
-                        var content = this.nextElementSibling;
-                        if (content.style.display === "block") {
-                        content.style.display = "none";
-                        } else {
-                        content.style.display = "block";
-                        }
-                    });
-                }
-            </script>
         </template>
 
     `,
     // methods related to the passage object
     methods: {
         // selects the text from the document when the user clicked on the related passage object
-        selectText(node) {
-            node = document.getElementById(node);
-            if (document.body.createTextRange) {
-                const range = document.body.createTextRange();
-                range.moveToElementText(node);
-                range.select();
-            } else if (window.getSelection) {
-                const selection = window.getSelection();
-                const range = document.createRange();
-                range.selectNodeContents(node);
-                selection.removeAllRanges();
-                selection.addRange(range);
-            } else {
-                console.warn("Could not select text in node: Unsupported browser.");
-            }
+        // selectText(node) {
+        //     node = document.getElementById(node);
+        //     if (document.body.createTextRange) {
+        //         const range = document.body.createTextRange();
+        //         range.moveToElementText(node);
+        //         range.select();
+        //     } else if (window.getSelection) {
+        //         const selection = window.getSelection();
+        //         const range = document.createRange();
+        //         range.selectNodeContents(node);
+        //         selection.removeAllRanges();
+        //         selection.addRange(range);
+        //     } else {
+        //         console.warn("Could not select text in node: Unsupported browser.");
+        //     }
+        // },
+
+
+
+        selectText(note) {
+            console.log("note");
+            console.log(note);
+            document.getElementById("content").scrollTo(0, note.yPosition);
+
+            const range = new Range();
+            console.log("note.parentnode");
+            console.log(note.parentNode);
+
+            console.log("selectRange");
+            console.log(note.selectRange);
+
+            range.setStart(note.startNode.firstChild, note.startOffset);
+            range.setEnd(note.endNode.firstChild, note.endOffset);
+
+            console.log("range");
+            console.log(range);
+
+            let select = window.getSelection();
+            select.removeAllRanges();
+            select.addRange(range);
+            console.log("select");
+            console.log(select);
         },
-        change(value,index) {
+
+        // selectText(note) {
+        //     document.getElementById("document").scrollTo(0, note.yPosition);
+
+        //     const newRange = new Range();
+        //     newRange.setStart(note.startNode.firstChild, note.startOffset);
+        //     newRange.setEnd(note.endNode.firstChild, note.endOffset);
+        //     let selection = window.getSelection();
+        //     selection.removeAllRanges();
+        //     selection.addRange(newRange);
+        // },
+
+
+        change(value, index) {
             noteObject = {
                 note: value,
-                i : index
+                i: index
             }
             this.$emit('update', noteObject);
         },
         del(index) {
             this.$emit('del', index);
+        },
+        toggle() {
+            this.toggled = !this.toggled;
+            console.log(this.toggled);
         }
     }
 
--- frontend/viewer/prototype.js
+++ frontend/viewer/prototype.js
@@ -6,28 +6,67 @@ const app = Vue.createApp({
             index: 0,
             isDisabled: true,
             currentFile: 0,
-            savedProperty : ""
+            savedProperty: ""
         }
     },
     methods: {
         // add a passage object
+        // addAnnotation() {
+        //     const selection = window.getSelection();
+        //     const string = selection.toString();
+        //     const range = selection.getRangeAt(0);
+        //     const frag = range.extractContents();
+        //     // const newnode = document.createElement("span");
+        //     // newnode.setAttribute("id", this.index);
+        //     // newnode.appendChild(frag);
+        //     range.insertNode(newnode);
+
+        //     // create a new js object with current selection 
+        //     const note = {
+        //         id: this.index,
+        //         locId: this.index,
+        //         selectRange: range,
+        //         rangeLength: range.length,
+        //         passage: string,
+        //         annotation: "",
+        //         fileId: this.currentFile,
+        //         file: undefined
+        //     };
+
+        //     if (string != "") {
+        //         this.notes.push(note);
+        //         this.index++;
+        //     }
+        // },
+
         addAnnotation() {
             const selection = window.getSelection();
             const string = selection.toString();
-            const range = selection.getRangeAt(0);
-            const frag = range.extractContents();
-            const newnode = document.createElement("span");
-            newnode.setAttribute("id", this.index);
-            newnode.appendChild(frag);
-            range.insertNode(newnode);
+            const range = window.getSelection().getRangeAt(0);
+            const selectionNode = window.getSelection().getRangeAt(0).startContainer.parentNode;
+            const doc = document.getElementById("content");
+            const selectionPosition = doc.scrollTop;
 
-            // create a new js object with current selection 
+            const startNode = window.getSelection().getRangeAt(0).startContainer.parentNode;
+            const endNode = window.getSelection().getRangeAt(0).endContainer.parentNode;
+
             const note = {
                 id: this.index,
+                locId: this.index,
+                rangeStart: selection.anchorOffset,
+                rangeLength: string.length,
+                startParentNode: selectionNode.childNodes[0],
+                parentNode: selectionNode,
+                yPosition: selectionPosition,
                 passage: string,
                 annotation: "",
-                fileId : this.currentFile,
-                file: undefined
+                fileId: this.currentFile,
+                file: undefined,
+
+                startOffset: range.startOffset,
+                endOffset: range.endOffset,
+                startNode: startNode,
+                endNode: endNode
             };
 
             if (string != "") {
@@ -35,6 +74,31 @@ const app = Vue.createApp({
                 this.index++;
             }
         },
+
+        // addAnnotation() {
+        //     const selection = window.getSelection();
+        //     const string = selection.toString();
+        //     const selectionNode = window.getSelection().getRangeAt(0).startContainer.parentNode;
+        //     const doc = document.getElementById("content");
+        //     const selectionPosition = doc.scrollTop;
+        //     const range = window.getSelection().getRangeAt(0);
+        //     const startNode = window.getSelection().getRangeAt(0).startContainer.parentNode;
+        //     const endNode = window.getSelection().getRangeAt(0).endContainer.parentNode;
+
+
+        //     const note = {
+        //         id: this.index,
+        //         locId: this.index,
+        //         startNode: startNode,
+        //         endNode: endNode,
+        //         startOffset: range.startOffset,
+        //         endOffset: range.endOffset,
+        //         passage: string,
+        //         annotation: "",
+        //         fileId: this.currentFile,
+        //         file: undefined
+        //     }
+        // },
 
         // send the entire passage object to the server
         async sendToServer() {
@@ -76,12 +140,11 @@ const app = Vue.createApp({
         deletePassage(index) {
             console.log(index);
             this.notes.splice(index, 1);
-            this.index -= 1;
             for (let i = 0; i < this.notes.length; i++) {
-                this.notes[i].id = i;   
+                this.notes[i].locId = i;
             }
         },
-        
+
         computed: {
             inPageAnchor() {
                 return '#' + this.index;
